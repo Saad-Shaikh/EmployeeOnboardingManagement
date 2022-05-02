a72eb7f4e2dc433aaf61d1cac143dbcc5bdf42a7
Saad Shaikh <saadshk111@gmail.com>
2022-05-02 09:46:40 +0530

    [ON-118] fix: remove unused imports from EmployeeSearchDTO

--- src/main/java/com/training/EmployeeOnboardingManagement/dto/EmployeeSearchDTO.java
+++ src/main/java/com/training/EmployeeOnboardingManagement/dto/EmployeeSearchDTO.java
@@ -1,10 +1,7 @@
 package com.training.EmployeeOnboardingManagement.dto;
 
-import com.training.EmployeeOnboardingManagement.enums.Designation;
-import com.training.EmployeeOnboardingManagement.enums.EmployeeStatus;
 import lombok.Data;
 
-// just for learning
 @Data
 public class EmployeeSearchDTO {
     private String name;
